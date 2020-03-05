a0430b1514d1436fbb765d447a2449665b525997
Shubhendu kushwah <skushwah@bestpeers.com>
2020-03-05 18:23:46 +0530

    Jspratice

--- myJs.js
+++ myJs.js
@@ -1,67 +1,115 @@
 $(document).ready(function(){
 
     $("#addItem").click(function(){
+        $("#inputName").show();
+        $("#inputAge").show();
+       
+        $("#readName").hide();
+        $("#readAge").hide();
+
+        $("#update2").hide();
+        $("#add").show();
         $("input").val('');
     });
 
     var sno = 1
-        $("#add").click(function(){
-
-           
+    // add row
+        $("#add").click(function(){           
             var name = $("#inputName").val();
             var age =  $("#inputAge").val();
-            var row = '<tr><th scope="row">'+sno+'</th><td class="name">'+
-            name+'</td><td class="age">'+
-            age+'</td><td>'+
-            '<div class="btn-group btn-group-toggle" data-toggle="buttons">\
-            <button type="button" id="update1" class="btn btn-primary updateItems" data-toggle="modal" data-target="#addModal">Update</button>\
-            <button type="button" id="" class="btn btn-primary" data-toggle="modal" >Up</button>\
-            <button type="button" id="" class="btn btn-primary" data-toggle="modal" >Down</button>\
-            <button type="button" id="delete" class="btn btn-primary" data-toggle="modal">Delete</button>\
-            <button type="button" id="" class="btn btn-primary" data-toggle="modal" >View</button>\
-          </div>'+'</td></tr>';
-            $('#tabody').append(row);
 
-            sno +=1;
+            if(isNaN(age)){
+                alert("Invalid age");
+
+            }
+            else{
+                var row = '<tr><th scope="row">'+sno+'</th><td class="name">'+
+                name+'</td><td class="age">'+
+                age+'</td><td>'+
+                '<div class="btn-group btn-group-toggle" data-toggle="buttons">\
+                <button type="button" class="btn btn-sm btn-dark update1" data-toggle="modal" data-target="#addModal">Update</button>\
+                <button type="button" id="" class="btn btn-dark" data-toggle="modal" >Up</button>\
+                <button type="button" id="" class="btn btn-dark" data-toggle="modal" >Down</button>\
+                <button type="button" id="delete" class="btn btn-dark" data-toggle="modal">Delete</button>\
+                <button type="button"  class="btn btn-dark view" data-toggle="modal" data-target="#addModal">View</button>\
+              </div>'+'</td></tr>';
+                $('#tabody').append(row);
+                sno +=1;
+    
+            }
+
+           
         });
-            // $("td");
 
-            // $("#fullName").text(name);
-            // $("#age").text(age);
-
-            // $("#")
-
-
-
+        // delete row
             $("#tabody").on('click', '#delete', function () {
                 $(this).closest('tr').remove();
             });
-            
-        $("#tabody").on("click",'#update1' ,function(){                
-            var name =$(event.target).closest('tr').find('.name').text();
-            var age = $(event.target).closest('tr').find('.age').text();
-            console.log(name,age );
+
+        var temp ='';
+
+        // open modal and fill current selected row data
+        $("#tabody").on("click",'.update1' ,function(){  
+                  
+            $("#add").hide();
+            $("#update2").show();
+            var name =$(this).closest('tr').find('.name').text();
+            var age = $(this).closest('tr').find('.age').text();
+
+
+            $("#inputName").show();
+            $("#inputAge").show();
+
+            $("#readName").hide();
+            $("#readAge").hide();
+
             $("#inputName").val(name);
             $("#inputAge").val(age);
 
-            $("#addModal").on("click",'#update2',function(){
-
-                var newname = $("#inputName").val();
-                var newage =  $("#inputAge").val();
-                console.log("testing",newname)
-
-                $(event.target).closest('tr').find('.name').text(newname);
-                $(event.target).closest('tr').find('.age').text(newage);
-            });
-            
-            
-            // $("#inputAge").val($("#ipage").text());
-            // $(this).click(function(){
-            //     var name = $("#ipname").text()
-            //     console.log(name); 
-            // });
-
+            temp = this;
             
         });
+
+        // fill updated data in modal
+        $("#update2").click(function(){
+                
+            $(temp).closest('tr').find('.name').text(getData()[0]);
+            $(temp).closest('tr').find('.age').text(getData()[1]);
+        });
+
+
+
+               // View
+               $("#tabody").on("click",'.view' ,function(){        
+                $("#add").hide();
+                $("#update2").hide();
+                var name =$(temp).closest('tr').find('.name').text();
+                var age = $(temp).closest('tr').find('.age').text();
+                
+                $("#inputName").hide();
+                $("#inputAge").hide();
+
+                $("#readName").show();
+                $("#readAge").show();
+               
+                $("#readName").val(name);
+                $("#readAge").val(age);
+                
+                
+                
+            });
+
+        function getData() {
+
+            var newname = $("#inputName").val();
+            var newage =  $("#inputAge").val();
+            var arr = [newname,newage]
+            return arr;
+        }
+
+        function validateModal(){
+
+            alert("Hola")
+        }
        
 });
